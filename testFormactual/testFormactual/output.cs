2edd56419cd8c320f5bed785f0e61f901d102d20
mewmew271 <37979148+mewmew271@users.noreply.github.com>
2018-11-22 13:27:04 +0000

    Add files via upload

--- testFormactual/testFormactual/output.cs
+++ testFormactual/testFormactual/output.cs
@@ -12,11 +12,13 @@ namespace testFormactual
 {
     public partial class output : Form
     {
-        public output(string name)
+        public output(string name, string ID)
         {
             InitializeComponent();
-            label1.Text = name;
-        }
+            outputFilmName.Text = name;
+			outputFilmID.Text = ID;
+			outputFilmDirector.Text = "bob";
+		}
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -24,5 +26,28 @@ namespace testFormactual
             
             
         }
-    }
+
+		private void outputFilmID_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void saveToWl_Click(object sender, EventArgs e)
+		{
+			wishlist wishlist = new wishlist(outputFilmName.Text, outputFilmID.Text, outputFilmDirector.Text);
+			//string text = outputFilmName.Text outputFilmID.Text outputFilmDirector.Text;
+
+			string[] wlSave = { outputFilmName.Text, outputFilmID.Text, outputFilmDirector.Text};
+			using (System.IO.StreamWriter file =
+					   new System.IO.StreamWriter(@"C:\Users\Student\Desktop\WriteText.txt"))
+			{
+				foreach (string line in wlSave)				
+				file.WriteLine(line);
+			}
+
+			//System.IO.File.WriteAllText(@"C:\Users\Student\Desktop\WriteText.txt", text);
+			this.Close();
+			//wishlist.Show();
+		}
+	}
 }
