cd4ec777ed9d9aa9775507e60ebf014b729e663a
mewmew271 <37979148+mewmew271@users.noreply.github.com>
2018-11-22 14:08:53 +0000

    Update Opening a web page.py

--- Opening a web page.py
+++ Opening a web page.py
@@ -5,7 +5,7 @@ br.implicitly_wait(15) # wait's for the page to get done loading before it does 
 br.get('http://www.google.com/')
 # to fill out a form
 search = br.find_element_by_name('q')
-search.send_keys('http://www.omdbapi.com/')#enter what ever you want the search results to be
+search.send_keys('http://www.omdbapi.com/?apikey=55be8851&')#enter what ever you want the search results to be
 search.submit()
 time.sleep(5)
 print(br.title)
